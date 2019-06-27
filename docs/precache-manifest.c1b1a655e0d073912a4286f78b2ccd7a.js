8d960ba2a4b18e95ac1db4370b2e86c76ad3f79b
mohan-cao <mohancao@yahoo.com.au>
2019-06-28 01:19:50 +1200

    set up gh-pages

---
+++ docs/precache-manifest.c1b1a655e0d073912a4286f78b2ccd7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10617fa9f22c13c3f6abf7e960bb55cc",
+    "url": "/mix-companion/index.html"
+  },
+  {
+    "revision": "55d4c3cbcaa9fad104d4",
+    "url": "/mix-companion/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "498261ef63e82ff0b2b4",
+    "url": "/mix-companion/static/js/2.3cdbdd3e.chunk.js"
+  },
+  {
+    "revision": "55d4c3cbcaa9fad104d4",
+    "url": "/mix-companion/static/js/main.0c2c430c.chunk.js"
+  },
+  {
+    "revision": "dc9ce5a2d0fa5400f329",
+    "url": "/mix-companion/static/js/runtime~main.5d2f421f.js"
+  }
+]);
